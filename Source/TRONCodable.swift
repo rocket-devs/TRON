cdf147447ad7598609710045fd1eb7c7e3eba912
Marcus Kida <marcus.kida@bearologics.com>
2025-06-12 15:20:52 +0200

    Fix Swift 5 compile issues

--- Source/TRONCodable.swift
+++ Source/TRONCodable.swift
@@ -136,10 +136,10 @@ open class CodableSerializer {
     let tron: TRON
 
     /// Decoder to be used while parsing model.
-    open let modelDecoder: JSONDecoder
+    open var modelDecoder: JSONDecoder
 
     /// Decoder to be used while parsing error.
-    open let errorDecoder: JSONDecoder
+    open var errorDecoder: JSONDecoder
 
     /// Creates `CodableSerializer` with `tron` instance to send requests, and `decoder` to be used while parsing response.
     init(_ tron: TRON,
--- Source/URLBuilder.swift
+++ Source/URLBuilder.swift
@@ -31,7 +31,7 @@ import Foundation
 open class URLBuilder: URLBuildable {
 
     /// Base URL string
-    open let baseURLString: String
+    open var baseURLString: String
 
     /**
      Initialize URL builder with Base URL String
